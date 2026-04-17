7d0fe073d45936fbcebc430dd411dc4ea105ceb5
ghuy311 <hogiahy311@gmail.com>
2026-04-17 16:30:08 +0700

    config payos

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -10,4 +10,8 @@ module.exports = {
   SUPABASE_URL: process.env.SUPABASE_URL || "",
   SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || "",
   SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || "",
+  PAYOS_CLIENT_ID: process.env.PAYOS_CLIENT_ID || "",
+  PAYOS_API_KEY: process.env.PAYOS_API_KEY || "",
+  PAYOS_CHECKSUM_KEY: process.env.PAYOS_CHECKSUM_KEY || ""
 };
+  
