dbb3c9b1a609fe3ddae23446f20d364260f9a229
Quang Dao HCMUS <tqdao21@clc.fitus.edu.vn>
2026-04-25 21:02:44 +0700

    Middleware phan quyen sale

--- backend/src/middlewares/sale.middleware.js
+++ backend/src/middlewares/sale.middleware.js
@@ -1,1 +1,16 @@
-print("Test branching Github")
+const { AppError } = require("../utils/errors");
+const { hasAnyRole } = require("../utils/roleUtils");
+
+// Định nghĩa các role được quyền truy cập module Sale
+const SALE_ACCESS_ROLES = ["ADMIN", "NHAN_VIEN", "SALE"]; 
+
+function requireSaleAccess(req, res, next) {
+  if (!req.user || !hasAnyRole(req.user.role, SALE_ACCESS_ROLES)) {
+    return next(new AppError("Forbidden - Cần quyền truy cập phân hệ Sale", 403));
+  }
+  return next();
+}
+
+module.exports = {
+  requireSaleAccess,
+};
